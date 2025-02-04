6636f25805c6d576ab307345ffb3c2943e268c85
1Kazzan <kauancarneiro034@gmail.com>
2025-02-04 20:16:34 -0300

    adicionando simulação

--- Alarm.c
+++ Alarm.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
-#include "pico/time.h" 
+#include "pico/stdlib.h" // biblioteca padrão para trabalhar com a raspberry pi pico
+#include "pico/time.h" // Biblioteca para gerenciamento de temporizadores e alarmes.
 
 // definir pinos GPIOs para os leds rgb e butão
 #define led_green 11
@@ -8,21 +8,21 @@
 #define led_red 13
 #define button 5
 
-bool leds_active = false;
-int estado = 1;
+bool leds_active = false; // indica que os leds estão acessos
 
+// função de callback para desligar o led vermelho após o tempo programado.
 int64_t turn_off_led_red_callback(alarm_id_t id, void *user_data) {
     gpio_put(led_red, 0);
-    leds_active = false;  // Permite nova ativação pelo botão
     return 0;
 }
 
+// função de callback para desligar o led azul após o tempo programado.
 int64_t turn_off_led_blue_callback(alarm_id_t id, void *user_data) {
     gpio_put(led_blue, 0);
-    leds_active = false;  // Permite nova ativação pelo botão
     return 0;
 }
 
+// função de callback para desligar o led verde após o tempo programado.
 int64_t turn_off_led_green_callback(alarm_id_t id, void *user_data) {
     gpio_put(led_green, 0);
     leds_active = false;  // Permite nova ativação pelo botão
@@ -49,18 +49,29 @@ int main()
     gpio_init(button);
     gpio_set_dir(button, GPIO_IN);
     gpio_pull_up(button);
+
  while (true) {
+    // verifica se o botão foi pressionado e se os leds ainda não estão ativos
     if (gpio_get(button) == 0 && !leds_active) {
-        sleep_ms(50);
+        sleep_ms(50); // debounce
+
+        // ativa os leds se o botão estiver pressionado
          if (gpio_get(button) == 0) {
             leds_active = true;
+
+            //liga os leds
             gpio_put(led_red, 1);
             gpio_put(led_blue, 1);
-             gpio_put(led_green, 1);
-            add_alarm_in_ms(3000, turn_off_led_green_callback, NULL, false);
+            gpio_put(led_green, 1);
+
+            // Configura alarmes para desligar os LEDs após os tempos definidos
+            add_alarm_in_ms(3000, turn_off_led_red_callback, NULL, false);
+            add_alarm_in_ms(6000, turn_off_led_blue_callback, NULL, false);
+            add_alarm_in_ms(9000, turn_off_led_green_callback, NULL, false);
             }
         }
-        sleep_ms(10);
+        // Pausa de 10 segundos para reduzir o uso da CPU.
+        sleep_ms(10000);
     }
     return 0;
     }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Alarm
+<h1 align="center">Embarcatech | Atividade de Interrupções </h1>
+
+Nome: Kauan Carneiro Almeida 
